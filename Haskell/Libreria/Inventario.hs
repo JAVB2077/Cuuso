1ee272f1df988d6aa22dab710db51f16e21fb5e5
EliseoMD <144739233+EliseoMD@users.noreply.github.com>
2025-03-21 12:54:34 -0600

    Update Inventario.hs

--- Haskell/Libreria/Inventario.hs
+++ Haskell/Libreria/Inventario.hs
@@ -3,7 +3,7 @@ import System.IO
 import System.Directory (removeFile, renameFile)
 import Data.List (delete)
 
-module Iventario (cargarInventario,guardarInvertario,agregarLibro,eliminarLibro,buscarLibro,buscarLibroPorTitulo,buscarLibroPorAutor,listaInventario,actulizarPrecioLibro,ordenarInventario,exportarInforme) where
+module Iventario (cargarInventario, guardarInvertario, agregarLibro,eliminarLibro, buscarLibro, buscarLibroPorTitulo, buscarLibroPorAutor, listaInventario, actulizarPrecioLibro, ordenarInventario, exportarInforme) where
 
 data Inventario = Inventario
   { libros :: [(Libro, Integer)]
@@ -22,16 +22,16 @@ buscarLibroPorTitulo inventario tituloBuscado = filter (\(libro, _) -> titulo li
 buscarLibroPorAutor :: Inventario -> String -> Inventario
 buscarLibroPorAutor inventario autorBuscado = filter (\(libro, _) -> autor libro == autorBuscado) inventario
 
---Eliminar libro
+  --Eliminar libro
 eliminarLibro :: FilePath -> Inventario -> String -> String -> IO Inventario
-eliminarLibro archivo (Inventario libros) tituloBuscado autorBuscado = do
-  let nuevoInventario = filter (\(libro, _) -> titulo libro /= tituloBuscado || autor libro /= autorBuscado) libros
-  -- Guardar el nuevo inventario en un archivo temporal
-  (tempName, tempHandle) <- openTempFile "." "temp"
-  hPutStr tempHandle $ unlines (map (\(libro, cantidad) -> titulo libro ++ "," ++ autor libro ++ "," ++ show (año libro) ++ "," ++ show (precio libro) ++ "," ++ show cantidad) nuevoInventario)
-  hClose tempHandle
-  -- Reemplazar el archivo original con el temporal
-  removeFile archivo
-  renameFile tempName archivo
-  putStrLn "Libro eliminado."
-  return (Inventario nuevoInventario)
+eliminarLibro archivo (Inventario listaLibros) tituloBuscado autorBuscado = do
+    let nuevoInventario = filter (\(libro, _) -> titulo libro /= tituloBuscado || autor libro /= autorBuscado) listaLibros
+    -- Guardar el nuevo inventario en un archivo temporal
+    (nombreTemp, manejadorTemp) <- openTempFile "." "temp"
+    hPutStr manejadorTemp $ unlines (map (\(libro, cantidad) -> titulo libro ++ "," ++ autor libro ++ "," ++ show (año libro) ++ "," ++ show (precio libro) ++ "," ++ show cantidad) nuevoInventario)
+    hClose manejadorTemp
+    -- Reemplazar el archivo original con el temporal
+    removeFile archivo
+    renameFile nombreTemp archivo
+    putStrLn "Libro eliminado."
+    return (Inventario nuevoInventario)
