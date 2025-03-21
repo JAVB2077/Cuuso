29bc6b7a4701ebad028206f9117deeab96a82899
EliseoMD <144739233+EliseoMD@users.noreply.github.com>
2025-03-20 23:38:29 -0600

    main.hs

--- Haskell/Libreria/LibreriaProf/main.hs
+++ Haskell/Libreria/LibreriaProf/main.hs
@@ -1,6 +1,32 @@
-module Main where
-
 import Libro
 import Inventario
 import Reportes
 
+module Main where
+
+main :: IO ()
+main = do
+  -- Cargar el inventario desde un archivo
+  inventario <- cargarInventario "inventario.txt"
+
+  -- Listar el inventario
+  putStrLn "Inventario de libros:"
+  listarInventario inventario
+
+  -- Agregar un nuevo libro
+  let nuevoLibro = crearLibro "Nuevo Libro" "Autor Nuevo" 2023 29.99
+  let inventarioConNuevoLibro = agregarLibro nuevoLibro 5 inventario
+  guardarInventario "inventario.txt" inventarioConNuevoLibro
+  putStrLn "\nLibro agregado:"
+  listarInventario inventarioConNuevoLibro
+
+  -- Buscar un libro por título
+  putStrLn "\nBuscando el libro 'Nuevo Libro' por título:"
+  let resultadosTitulo = buscarPorTitulo inventarioConNuevoLibro "Nuevo Libro"
+  listarInventario resultadosTitulo
+
+  -- Eliminar un libro del inventario por título y autor
+  putStrLn "\nEliminando el libro 'Nuevo Libro' del autor 'Autor Nuevo':"
+  inventarioActualizado <- eliminarLibro "inventario.txt" inventarioConNuevoLibro "Nuevo Libro" "Autor Nuevo"
+  putStrLn "\nInventario actualizado:"
+  listarInventario inventarioActualizado
