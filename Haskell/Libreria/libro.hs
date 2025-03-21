1b0f764915b585f3be60edbbe1e09fdc4368613b
EliseoMD <144739233+EliseoMD@users.noreply.github.com>
2025-03-21 11:51:51 -0600

    Delete Haskell/Libreria/libro.hs

--- Haskell/Libreria/libro.hs
+++
@@ -1,8 +0,0 @@
-module Libro (Libro(..)) where
-
-data Libro =Libro { titulo :: String
-                , autor :: String
-                , anioPublicacion :: Int
-                , precio :: Float
-                , inventario :: Int
-                    } deriving (Show,Eq)
