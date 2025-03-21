80be3da968de247e13497c6eb40266b90b7f38ca
JAVB2077 <143929200+JAVB2077@users.noreply.github.com>
2025-03-20 19:28:33 -0600

    generarReporte.hs comentado al completo

--- Haskell/Libreria/metodos.hs
+++ Haskell/Libreria/metodos.hs
@@ -1,2 +1,15 @@
-module Metodos (cargarInventario,guardarInvertario,agrgegarLibro,eliminarLibro,buscarLibro,eliminarLibro,buscarLibro,listaInventario,actulizarPrecioLibro,ordenarInventario,exportarInforme) where
+module Metodos (cargarInventario,guardarInvertario,agregarLibro,eliminarLibro,buscarLibro,eliminarLibro,buscarLibro,listaInventario,actulizarPrecioLibro,ordenarInventario,exportarInforme) where
 import Libro
+agregarLibro :: (String, String, Int, Float, Int) -> [(String, String, Int, Float, Int)] -> [(String, String, Int, Float, Int)]
+--agregarLibro libro listaLibros = libro : listaLibros
+agregarLibro (titulo, autor, anioPublicacion, precio, inventario) listaLibros = [(titulo, autor, anioPublicacion, precio, inventario)] ++ listaLibros
+
+imprimirDirectorio :: [(String, String, Int, Float, Int)] -> IO()
+imprimirDirectorio listaLibro = mapM_ (print) listaLibro
+
+
+buscarPorTitulo :: String -> [(String, String, Int, Float, Int)] -> [(String, String, Int, Float, Int)]
+buscarPorTitulo titulo listaLibros = filter (\(t, _, _, _, _) -> t == titulo) listaLibros
+
+buscarPorAutor :: String -> [(String, String, Int, Float, Int)] -> [(String, String, Int, Float, Int)]
+buscarPorAutor autor listaLibros = filter (\(_, a, _, _, _) -> a == autor) listaLibros
